ed70ed0433c08fd0a9458ccb77f354a4656b63b1
Ronald G. Minnich <rminnich@gmail.com>
2022-03-29 15:36:28 -0700

    Initial copy step ready
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/u-root/sourcery
 
 go 1.18
+
+require (
+	github.com/hashicorp/go-multierror v1.1.1
+	github.com/u-root/uio v0.0.0-20220204230159-dac05f7d2cb4
+)
+
+require github.com/hashicorp/errwrap v1.0.0 // indirect
--- main.go
+++ main.go
@@ -2,36 +2,83 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build go1.18
+// +build go1.18
+
 package main
 
-func tree(args ...string) error {
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/u-root/uio/cp"
+)
+
+var (
+	home string
+	V    = log.Printf
+)
+
+func tree(gotoolchain string, args ...string) (string, error) {
 	d, err := os.MkdirTemp("", "sourcery")
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 	opts := cp.Options{
-		NoFollowSymlinks: f.noFollowSymlinks,
+		NoFollowSymlinks: true,
 	}
-
-	var err error
-	for _, file := range args {
-		if e := opts.CopyTree(file, d); e != nil {
+	err = nil
+	for i, dir := range append([]string{gotoolchain}, args...) {
+		b := "go"
+		if i > 0 {
+			b = filepath.Join("src", filepath.Base(dir))
+		}
+		if !filepath.HasPrefix(dir, home) {
+			err = multierror.Append(err, fmt.Errorf("%q does not have %q as a prefix", dir, home))
+		}
+		b = filepath.Join(d, b)
+		V("Copy %q -> %q", dir, b)
+		if e := opts.CopyTree(dir, b); e != nil {
 			err = multierror.Append(err, e)
 		}
 	}
+	return d, err
+}
+
+func env() error {
+	var (
+		ok  bool
+		err error
+	)
+	home, ok = os.LookupEnv("HOME")
+	if !ok {
+		err = multierror.Append(fmt.Errorf("$HOME is not set"))
+	}
 	return err
 }
+
 func main() {
 	flag.Parse()
 
+	if err := env(); err != nil {
+		log.Fatal(err)
+	}
 	a := flag.Args()
+	if len(a) == 0 {
+		log.Fatal("Usage: sourcery go-toolchain [args...]")
+	}
 	// Build the target directory
 	// Start with a temporary directory
 	// copy the toolchain there
 	// copy the rest of the other directories there
 
-	tree, err := tree(args...)
+	tree, err := tree(a[0], a[1:]...)
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Printf("The tree is in %q", tree)
 }
